daa6652fae4db1f31151ab7146a25efd4d45a434
magnus <578031@hvl.stud.no>
2021-10-06 13:31:12 +0200

    finished

---
+++ WebAPIGuide/src/main/java/com/example/WebAPIGuide/WebApiGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebAPIGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiGuideApplication.class, args);
+	}
+
+}
---
+++ WebAPIGuide/src/test/java/com/example/WebAPIGuide/WebApiGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebAPIGuide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApiGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
